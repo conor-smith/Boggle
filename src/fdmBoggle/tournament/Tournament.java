379dffbfb81cb2a596d8d7f558d9e8ffe398927b
conor-smith <conormartindale@gmail.com>
2020-08-13 16:16:10 +1000

    Removed TestPlayers from commit Added extensive exception handling to tournament. All Exception handling has been tested

--- src/fdmBoggle/tournament/Tournament.java
+++ src/fdmBoggle/tournament/Tournament.java
@@ -21,27 +21,27 @@ public class Tournament
         BogglePlayer[] players = new BogglePlayer[playerClassFiles.length];
         Boggle[] games = new Boggle[noOfGames];
         
-        File resultsFile = new File(System.getProperty("user.home") + "/Documents/tournament.csv");
-        FileWriter output = null;
-        
         System.out.println("Loading players");
         
         try
         {
-            if(!resultsFile.exists())
-            {
-                System.out.println("Creating results file at " + resultsFile);
-                resultsFile.createNewFile();
-            }
-
-            output = new FileWriter(resultsFile);
+            
             classLoader = new URLClassLoader(new URL[] {new URL("file://" + System.getProperty("user.home") + "/Documents/players/")});
             for(int i = 0;i < playerClassFiles.length;i++)
             {
-                String className = playerClassFiles[i].getName().split("\\.")[0];
-                @SuppressWarnings("unchecked")
-                Class<BogglePlayer> playerClass = (Class<BogglePlayer>) classLoader.loadClass(className);
-                players[i] = playerClass.getDeclaredConstructor().newInstance();
+                try
+                {
+                    String className = playerClassFiles[i].getName().split("\\.")[0];
+                    @SuppressWarnings("unchecked")
+                    Class<BogglePlayer> playerClass = (Class<BogglePlayer>) classLoader.loadClass(className);
+                    players[i] = playerClass.getDeclaredConstructor().newInstance();
+                }
+                catch(Exception e)
+                {
+                    String[] fullPath = playerClassFiles[i].getName().split("/");
+                    System.out.println(fullPath[fullPath.length - 1] + " is either not a class file or does not meet tournament standards");
+                    players[i] = null;
+                }
             }
         }
         catch(Exception e)
@@ -49,18 +49,25 @@ public class Tournament
             System.out.println(e.getMessage());
             return;
         }
-
+        
         System.out.println("Creating game boards.");
-
+        
         BoggleDictionary.init();
-
+        
         for(int i = 0;i < noOfGames;i++)
             games[i] = new Boggle(true);
         
         System.out.println("Beginning tournament");
-
+        
         try
         {
+            File resultsFile = new File(System.getProperty("user.home") + "/Documents/tournament.csv");
+            FileWriter output = new FileWriter(resultsFile);
+            if(!resultsFile.exists())
+            {
+                System.out.println("Creating results file at " + resultsFile);
+                resultsFile.createNewFile();
+            }
             output.write("Player, ");
             for(int i = 0;i < noOfGames;i++)
                 output.write("Game" + i + ", , , ");
@@ -81,25 +88,34 @@ public class Tournament
         
             for(BogglePlayer player : players)
             {
-                if(player != null)
+                try
                 {
-                    output.write(player.toString() + ", ");
-
-                    player.buildDictionary(new File(System.getProperty("user.home") + "/Documents/data/dict.txt"));
-                    
-                    for(Boggle game : games)
+                    if(player != null)
                     {
-                        long startTime = System.currentTimeMillis();
-                        player.playGame(game);
-                        long endTime = System.currentTimeMillis();
-                        long timeTaken = endTime - startTime;
+                        output.write(player.toString() + ", ");
 
-                        output.write(game.getScore() + ", " + timeTaken + ", " + ((double)game.getScore() / (double)timeTaken) + ", ");
+                        player.buildDictionary(new File(System.getProperty("user.home") + "/Documents/data/dict.txt"));
                         
-                        System.out.println("Player " + player.toString() + " took " + timeTaken + " milliseconds to get " + game.getScore() + " points");
-                        game.resetScoreAndGuesses();
+                        for(Boggle game : games)
+                        {
+                            long startTime = System.currentTimeMillis();
+                            player.playGame(game);
+                            long endTime = System.currentTimeMillis();
+                            long timeTaken = endTime - startTime;
+
+                            output.write(game.getScore() + ", " + timeTaken + ", " + ((double)game.getScore() / (double)timeTaken) + ", ");
+                            
+                            System.out.println("Player " + player.toString() + " took " + timeTaken + " milliseconds to get " + game.getScore() + " points");
+                            game.resetScoreAndGuesses();
+                        }
+                        
+                        output.write("\n");
                     }
-                    
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Player " + player.toString() + " threw the following exception");
+                    System.out.println(e.toString() + ": " + e.getMessage());
                     output.write("\n");
                 }
             }
