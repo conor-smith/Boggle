b7fc7e9e7f7c664e537548fc716b90301ec22075
conor-smith <conormartindale@gmail.com>
2020-08-14 13:52:22 +1000

    Added documentation for Boggle.java

--- src/fdmBoggle/game/Boggle.java
+++ src/fdmBoggle/game/Boggle.java
@@ -4,27 +4,55 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import fdmBoggle.ai.WordGetter;
 
+/**
+ * This object manages the game logic
+ * Is made to function both in tournament mode and game mode
+ * 
+ * @author Conor Smith
+ */
 public class Boggle
 {
-    //The game board with (0,0) at the top left corner
+    /** Stores the 4*4 board of 'dice'. Uses String to account for 'Qu' piece */
     private String[][] board;
+    /** Stores all correct words that have been entered. Ensures player cannot get points by entering the same word multiple times */
     private ArrayList<String> guessedWords;
+    /** 
+     * A set of all legal words for this game. Was used to make it easier during tournaments as it allows players to enter a String as opposed to a list of positions 
+     * Does take time to build
+    */
     private HashSet<String> legalWords;
+    /** The game score so far */
     private int score;
+
+    /** Signifies that this is a tournament */
     boolean tournamentMode;
     long tStartTime, tEndTime;
 
+    /** 
+     * Calls reset() Sets tournament mode to false
+     */
     public Boggle()
     {
         tournamentMode = false;
         reset();
     }
 
+    /**
+     * Calls reset()
+     * @param tournamentMode - sets tournamentMode property
+     */
     public Boggle(boolean tournamentMode)
     {
         this.tournamentMode = tournamentMode;
         reset();
     }
+
+    /** 
+     * Calls reset(String[][] board)
+     * @param tournamentMode - sets tounamentMode property
+     * @param board - passed into reset(String[][] board)
+     * @throws BoggleException - if reset(String[][] board) throws this exception
+     */
 
     public Boggle(String[][] board, boolean tournamentMode) throws BoggleException
     {
@@ -32,6 +60,10 @@ public class Boggle
         reset(board);
     }
 
+    /** 
+     * Calls BoardGenerator and WordGetter to generate board and legalWords
+     * If property tournamentMode is set to true, this object will record the time taken by the WordGetter
+     */
     public void reset()
     {
         board = BoardGenerator.generateBoard();
@@ -49,6 +81,11 @@ public class Boggle
         guessedWords = new ArrayList<String>();
     }
 
+    /**
+     * Sets param board to entered value
+     * @param board - sets board property
+     * @throws BoggleException - if board is of incorrect dimensions
+     */
     public void reset(String[][] board) throws BoggleException
     {
         if(board.length > 4 || board[0].length > 4)
@@ -59,12 +96,22 @@ public class Boggle
         guessedWords = new ArrayList<String>();
     }
 
+    /** 
+     * Sets param score to 0 and param guessedWords to an empty list
+     * Used to 'recycle' a board in tournament mode
+     */
     public void resetScoreAndGuesses()
     {
         this.guessedWords = new ArrayList<String>();
         this.score = 0;
     }
 
+    /**
+     * Used to enter a word
+     * If param tournamentMode is true, it subracts one point from param score for incorrect words
+     * @param word - A string representing a word that can be constructed with the board. Can be incorrect
+     * @return true if word is in legalWords but not in guessedWords, otherwise returns false
+     */
     public boolean enterWord(String word)
     {
         if(!guessedWords.contains(word) && legalWords.contains(word))
@@ -81,6 +128,10 @@ public class Boggle
         }
     }
 
+    /**
+     * Used in tournaments to provide data for the 'Default' player
+     * @return a 2-length array containing the time taken for wordGetter and the totalScore of all words in param legalWords
+     */
     public long[] getTimeAndScore()
     {
         long[] answer = new long[2];
@@ -91,6 +142,7 @@ public class Boggle
         return answer;
     }
 
+    /**Prints 4 * 4 prepresentation of param board to the CLI */
     public void printBoard()
     {
         for(int i = 0;i < board.length;i++)
@@ -101,16 +153,28 @@ public class Boggle
         }
     }
 
+    /**
+     * Gets board property 
+     * @return board
+     */
     public String[][] getBoard()
     {
         return board;
     }
 
+    /** 
+     * returns score property 
+     * @return score
+     */
     public int getScore()
     {
         return score;
     }
 
+    /**
+     * Returns score for an entered word based on it's length
+     * @return 1 if Word length = 3-4, 2 if 5, 3 if 6, 5 if 7, 11 if >=8
+     */
     private int getWordScore(String word)
     {
         if(word.length() <= 4)
